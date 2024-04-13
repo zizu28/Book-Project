ec2688e9fa32697c7b504e0ede41076e4f46f8b7
zizu28 <ziblimzulka.zz@gmail.com>
2024-04-13 21:18:14 +0700

    Minor alterations

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-function Book(title, author, coverUrl, intro){
+function Book(title, author, pages, coverUrl, intro, read = "Not read"){
     this.title = title;
     this.author = author;
+    this.pages = pages;
     this.coverUrl = coverUrl;
     this.intro = intro;
+    this.read = function(){
+      return read === "Not read" ? "Read" : "Not read";
+    }
 }
 
 function addBookCard(Book) {
     // Clone the template to avoid modifying the original structure
     const newBookCard = document.querySelector('.book-card').content.cloneNode(true);
   
-    // Set book title, author, and intro content
+    // Set book title, author, pages, and intro content
     newBookCard.querySelector(".book-title").textContent = Book.title;
     newBookCard.querySelector(".book-author").textContent = Book.author;
+    newBookCard.querySelector(".book-pages").textContent = Book.pages;
     newBookCard.querySelector(".book-intro").textContent = Book.intro;
+    
   
     // Set book cover image (if URL provided)
-    if (bookData.coverUrl) {
+    if (Book.coverUrl) {
       newBookCard.querySelector(".book-cover").src = Book.coverUrl;
     } else {
       // Add a placeholder or handle the case of no cover image
@@ -23,9 +29,16 @@ function addBookCard(Book) {
     }
   
     // Append the new book card to a container element in your HTML
-    document.querySelector(".book-card-container").appendChild(newBookCard);
+    document.querySelector(".bookS").appendChild(newBookCard);
   }
 
+  function isRead(Book){
+    const read = document.querySelector(".read");
+    if (Book.read() === read.textContent) {
+      read.textContent = Book.read();
+      this.read = "Read";
+    }
+  }
 const myLibrary = []
 
 
